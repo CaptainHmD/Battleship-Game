ee7175cffa1ab965249acc78f6fa38de05a52926
HaMaD <hamad.laaboun@gmail.com>
2022-09-17 19:38:38 +0300

    set up server with express framework

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const root = path.join(__dirname);
+const PORT = process.env.PORT ||3500;
+
+
+// !important 
+app.use(express.static('public'));
+
+
+
+// built in middleware to handle urlencoded from data
+app.use(express.urlencoded({extended:true}))
+
+// built in middleware for json
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+app.get('/test', (req, res) => {
+   res.sendFile(path.join('public','views','index.html'),{root:root})
+});
+
+app.listen(PORT, () => {
+  console.log('server is listing on Port: ',PORT);
+});
