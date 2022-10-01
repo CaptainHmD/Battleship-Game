61e74df9ee7143025cc677c37cf10db0dd959ca3
HaMaD <hamad.laaboun@gmail.com>
2022-10-02 00:37:33 +0300

    start working with drop event

--- public/js/script-game.js
+++ public/js/script-game.js
@@ -1,27 +1,45 @@
 
 const board = document.querySelector('#board'); //! game board
-function createBoard () {
-    for(let i = 0 ; i < 100; i ++){
-        const cell =  document.createElement('div');
-        cell.setAttribute('class','cell');
-        cell.setAttribute('data-cell','')
+createBoard();
+const cells = document.querySelectorAll('[data-cell]');
+var shipParts; //!
+const dragAbles = document.querySelectorAll('.drag');
+var lastCellHover; //!
+var shipSize; //!
+var numberOfTheShipPart;
+const EventDelay = 1000;
+const notAllowedHorizontalOnEnd = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
+const notAllowedHorizontalOnEStart = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+const notAllowedVertical = [90,]
+
+addEvents();
+
+
+// document.addEventListener("dragover", function(event) {
+//     event.preventDefault();
+//   });
+// document.addEventListener("drop", function(event) {
+//     event.preventDefault();
+//   });
+
+function createBoard() {
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement('div');
+        cell.setAttribute('class', 'cell');
+        cell.setAttribute('data-cell', '')
+        cell.innerHTML = i  //TODO:  delete later ******* 
         board.appendChild(cell);
 
     }
 }
-createBoard();
 
-const cells = document.querySelectorAll('[data-cell]');
-
-addEvent();
-const EventDelay = 2000;
-function addEvent() {
+function addEvents() {
     cells.forEach(cell => {
         cell.addEventListener('click', myClickHandler)
     }
     )
 }
-
 function removeEvent() {
     cells.forEach(cell => {
         cell.removeEventListener('click', myClickHandler)
@@ -35,9 +53,9 @@ function myClickHandler(handler) {
     if (tar.classList.contains('placed')) { // if the cell was clicked  don`t do anything
         return;
     }
-    console.log('clicked');
-    console.log("Parent:", Array.from(handler.currentTarget.parentNode.children).indexOf(handler.currentTarget))
-    tar.innerHTML = 'clicked'
+    console.log('clicked'); //TODO:  delete later ******* 
+    console.log("Parent:", Array.from(handler.currentTarget.parentNode.children).indexOf(handler.currentTarget)) //TODO:  delete later ******* 
+    tar.innerHTML = 'clicked' //TODO:  delete later ******* 
     place(tar)
     EventTimeHandler();
 
@@ -47,50 +65,70 @@ function EventTimeHandler() {
     removeEvent();
     setTimeout(() => {
         addEvent();
-
     }, EventDelay)
-
 }
-
-const place = (cell) => {
-    cell.classList.add('placed')
-
-}
-
-
-
-cells.item(2).innerHTML = 'here'
-
-//const cells = document.querySelectorAll('[data-cell]');
-
-// const board = document.querySelector('#board'); //! game board
-
-
-const dragAbles = document.querySelectorAll('.drag')
+const place = (cell) => {cell.classList.add('placed')}
 
 dragAbles.forEach(ships => {
-    ships.addEventListener('dragstart', ship => { // this will start when you dragging the ships 
-        console.log('dragging');
-        ships.classList.add('dragging'); // for some effects
-        console.log(ship.target);
-    })
-    ships.addEventListener('dragend', ship => {
-        ships.classList.remove('dragging')// to remove the effects
-        console.log(ship.target);
+    ships.addEventListener('dragstart', ship => {onDragStart(ship, ships)})
+    ships.addEventListener('dragend', onDragEnd(ships))
+    ships.addEventListener('mousedown', onMouseDown)
+}) //! end of forEach for dragAbles
 
-    })
-    ships.addEventListener('mousedown', handler => {
-        console.log('click:\t', (handler.target.id).split('-')[1]); //! for knowing where is the parts the has been clicked
-        // console.log(Array.from(handler.currentTarget.parentNode.children).indexOf(handler.currentTarget));
 
-    })
-})
+function onDragStart(ship, ships) {
+    ships.classList.add('dragging'); //* for some effects
+    console.log(ship.target);
+    shipParts = ship.path[0].children.length;//* to get number of  ships part 
+}
+function onDragEnd(ships) {
+    ships.classList.remove('dragging')//* to remove the effects
+}
+
+function onMouseDown(handler) {
+    shipSize = (handler.target.id).split('-')[0];
+    numberOfTheShipPart = (handler.target.id).split('-')[1]
+    console.log('size:\t', shipSize); //* for knowing what is the size of the ship that has been clicked
+    console.log('click:\t', numberOfTheShipPart); //* for knowing where is the parts the has been clicked
+}
+
+
+
 
 cells.forEach(cell => {
-    cell.addEventListener('dragover', handler => {
-        handler.preventDefault()// cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
-        console.log("handler \t\t", handler);
-        console.log('tar\n\n', handler.target);
-        console.log(Array.from(handler.currentTarget.parentNode.children).indexOf(handler.currentTarget));
-    })
-})
+    cell.addEventListener('dragover', dragOverTheCells)
+    cell.addEventListener('drop', addShipsIntoCells, true)
+})//! end of cells foreach for cells
+
+
+
+function addShipsIntoCells(ship) {
+    console.log('drop');
+    console.log(ship.target);
+    for (let i = 0; i < shipParts; i++) {
+        if (i === 0) {
+            cells.item(lastCellHover).classList.add('start')
+            cells.item(lastCellHover).classList.add('placed')
+
+        } else if (i === (shipParts - 1)) {
+            cells.item(lastCellHover + i).classList.add('end')
+        }
+        cells.item(lastCellHover + i).classList.add('placed')
+        cells.item(lastCellHover + i).classList.add('ship-placed')
+
+    } // end for loop
+
+
+}//end of addShipsIntoCells
+
+
+function dragOverTheCells(handler) {
+
+    handler.preventDefault()// cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
+    // console.log(handler);
+    if (handler.target.classList.contains('placed')) { // any cells that have placed class 
+        // console.log('tar\n\n', handler.target.classList.contains('placed'));
+    }
+    lastCellHover = Array.from(handler.currentTarget.parentNode.children).indexOf(handler.currentTarget)
+    // console.log(lastCellHover);
+} // end of dragOverTheCells
